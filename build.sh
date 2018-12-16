429cbdd700b7bac4211b759b3d7f6a70e4fb6c9e
Wouter Wijsman <wwijsman@live.nl>
2018-12-16 14:40:58 +0100

    Updated licenses file and moved source code to the src directory

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-set -e
+set -ex
+
+#Set the working directory
+WORKDIR="$(dirname "$0")"
+cd ${WORKDIR}
 
 # Make sure we're running in the correct directory
-cd "$(dirname "$0")"
-
 NAME="lovetris"
+SOURCEDIR="src"
 BUILDDIR="build"
+DEPSDIR="deps"
 LOVEFILE="${BUILDDIR}/${NAME}.love"
 PLATFORMS="win32 win64 lin32 lin64"
 
@@ -14,22 +18,22 @@ rm -rf ${BUILDDIR}
 mkdir -p ${BUILDDIR}
 
 #Create the love file
-zip -9 -r ${LOVEFILE} . -x .git/\* -x deps/\* -x build/ -x build.sh -x run.sh -x README.md
+cd ${SOURCEDIR} && zip -9 -r ../${LOVEFILE} . && cd ..
 
 #Build executables for all platforms and copy additional required files
 for platform in ${PLATFORMS}; do
 	mkdir -p ${BUILDDIR}/${platform}
 	#Create the executable
 	if [ "${platform}" == "win32" ] || [ "${platform}" == "win64" ]; then
-		cat deps/${platform}/love.exe ${LOVEFILE} > ${BUILDDIR}/${platform}/${NAME}.exe
+		cat ${DEPSDIR}/${platform}/love.exe ${LOVEFILE} > ${BUILDDIR}/${platform}/${NAME}.exe
 	else
-		cat deps/${platform}/love ${LOVEFILE} > ${BUILDDIR}/${platform}/${NAME}
+		cat ${DEPSDIR}/${platform}/love ${LOVEFILE} > ${BUILDDIR}/${platform}/${NAME}
 		#Set permissions
 		chmod +x ${BUILDDIR}/${platform}/${NAME}
 	fi
 	
 	#copy dependencies
-	cp deps/${platform}/copy/* ${BUILDDIR}/${platform}/
+	cp ${DEPSDIR}/${platform}/copy/* ${BUILDDIR}/${platform}/
 	
 	#Create a zip with everything you need to run the game
 	zip -9 -r ${BUILDDIR}/${NAME}-${platform}.zip ${BUILDDIR}/${platform}/
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 WD="$(dirname "$0")"
 cd ${WD}
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${WD}/deps/lin64/copy/"
-${WD}/deps/lin64/love .
+${WD}/deps/lin64/love src
